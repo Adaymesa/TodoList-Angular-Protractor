4264e87d903f4a658bfc10b53ec7029f8ebfcddf
Aday Mesa <adaymesagc@gmail.com>
2016-06-02 16:20:41 +0100

    Protractor issues fixed, up and running

---
+++ app/js/app.js
@@ -0,0 +1,1 @@
+var toDoApp = angular.module('toDoApp', []);
---
+++ test/e2e/app.spec.js
@@ -0,0 +1,7 @@
+describe("app", function() {
+  it("should get home page title", function() {
+    browser.get('/');
+    expect(browser.getTitle()).toEqual("Todos App");
+  });
+});
+
